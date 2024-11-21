121e45d156d2d134dbd8c90a24b5c74cfac07a06
AleXDE54 <93430311+AleXDE54@users.noreply.github.com>
2024-11-21 17:17:03 +0700

    modified:   config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ serverprompt = "" # prompt that the bot will obey.
 model = "mixtral-8x7b"
 
 language = "en"
+# PLEASE INSTALL ALPHA VERSION OF GOOGLE TRANSLATOR API
+# pip install googletrans==3.1.0a0
