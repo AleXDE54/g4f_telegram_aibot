173ce221773f08f6440374bc5e029046cb655503
AleXDE54 <93430311+AleXDE54@users.noreply.github.com>
2024-11-21 17:14:22 +0700

    modified:   moonlight.py

--- moonlight.py
+++ moonlight.py
@@ -1,16 +1,12 @@
 import g4f
 from config import serverprompt, model
-
 def ask(prompt) -> str:
-
     response = g4f.ChatCompletion.create(
         model=model,
         messages=([{"role": "system", "content": serverprompt}] + [{"role": "user", "content": prompt}]),
         stream=True,
     )
-
     ans_message = ''
     for message in response:
         ans_message += message
-
     return ans_message
