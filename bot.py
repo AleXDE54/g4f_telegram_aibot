e236be60330f1ca04c2507e8ecbb8bd2a3cc0d64
AleXDE54 <93430311+AleXDE54@users.noreply.github.com>
2024-11-23 06:16:25 +0700

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ import telebot
 from config import token, serverprompt
 from moonlight import ask, question, bot
 
-print('Bot started!')
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
     msg = bot.send_message(message.chat.id, '👋')
